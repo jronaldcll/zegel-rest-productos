d89fff344a0070894abeda2a0d7d47ac64476be7
Ronald Castillo <jronaldcll@gmail.com>
2024-05-11 08:50:34 -0500

    feat[ZEGEL]: Initial project rest with Java

---
+++ src/test/java/com/zegel/servicerestproduct/ServicerestproductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zegel.servicerestproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicerestproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
